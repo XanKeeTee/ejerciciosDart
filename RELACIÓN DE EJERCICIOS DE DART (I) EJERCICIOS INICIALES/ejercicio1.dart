0c149ff245b9fa99bb1c3e5b696d65d0ecfb5374
Miguel Angel Gambero <miguelangelgambero@gmail.com>
2026-09-24 19:04:07 +0200

    first commit

---
+++ RELACIÓN DE EJERCICIOS DE DART (I) EJERCICIOS INICIALES/ejercicio1.dart
@@ -0,0 +1,15 @@
+void main(){
+  //Imprimimos el hola mundo
+    print("Hola mundo!");
+  
+  //Inicializamos las 2 variables
+    int numero = 1;
+    String nombre = ("Miguel");
+
+  //Imprimimos las 2 variables que hemos creado
+    print(numero);
+    print(nombre);
+
+
+    // Para comentar son con las 2 barras
+}
